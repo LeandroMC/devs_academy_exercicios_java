e57c322e1d119aa2ca3ea85fcb53d93998b48772
LeandroMC <leandromc@pm.me>
2021-09-22 23:49:15 -0300

    add new exercices

--- _04_loops_e_arrays/_07.java
+++ _04_loops_e_arrays/_07.java
@@ -1,10 +1,29 @@
 package _04_loops_e_arrays;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class _07 {
 
     public static void main(String[] args) {
 
-        // 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144...
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Quantidade de termos:");
+        int n = input.nextInt();
+
+        int[] fibonacci = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            if (i < 2) {
+                fibonacci[i] = i;
+                continue;
+            }
+            fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+        }
+        System.out.println(Arrays.toString(fibonacci));
+
+        input.close();
     }
 
 }
